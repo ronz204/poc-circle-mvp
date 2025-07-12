633ac3081acdc2bc078e0f36d442a69d3f8169c5
roney ruiz <roneyruizrojas@gmail.com>
2025-07-11 23:03:40 -0600

    feat(server): create unit of work for repos

--- server/src/Infrastructure/Configs/Inversify.ts
+++ server/src/Infrastructure/Configs/Inversify.ts
@@ -1,4 +1,5 @@
 import { Container } from "inversify";
+import { PrismaClient } from "generated/prisma";
 import { UnitOfWork } from "@Database/UnitOfWork";
 
 import { AuthRegisterUseCase } from "@UseCases/Auth/AuthRegister/AuthRegisterUseCase";
@@ -7,4 +8,5 @@ export const Inversify = new Container();
 
 Inversify.bind(AuthRegisterUseCase).toSelf();
 
-Inversify.bind(UnitOfWork).toConstantValue(new UnitOfWork());
+Inversify.bind(UnitOfWork).toSelf();
+Inversify.bind(PrismaClient).toConstantValue(new PrismaClient());
--- server/src/Infrastructure/Repositories/UserRepository.ts
+++ server/src/Infrastructure/Repositories/UserRepository.ts
@@ -1,23 +1,21 @@
-import type { User } from "@Models/User";
 import { PrismaClient } from "generated/prisma";
-import type { IUserRepository } from "@Repositories/IUserRepository";
 
-export class UserRepository implements IUserRepository {
+export class UserRepository {
   constructor(private readonly prisma: PrismaClient) {};
 
-  async list(): Promise<User.Records> {
+  public async list() {
     return this.prisma.user.findMany();
   };
 
-  async findById(id: number): Promise<User.Entity | null> {
+  public async findById(id: number) {
     return this.prisma.user.findUnique({ where: { id } });
   };
 
-  async findByName(name: string): Promise<User.Entity | null> {
+  public async findByName(name: string) {
     return this.prisma.user.findUnique({ where: { name } });
   };
 
-  async findByEmail(email: string): Promise<User.Entity | null> {
+  public async findByEmail(email: string) {
     return this.prisma.user.findUnique({ where: { email } });
   };
 };
