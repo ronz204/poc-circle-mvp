4de1a1772f449463ee4dc60f89fade6db229f314
roney ruiz <roneyruizrojas@gmail.com>
2025-07-21 22:08:53 -0600

    feat(server): complete auth register use case

--- server/src/Application/UseCases/Auth/AuthRegister/AuthRegisterUseCase.ts
+++ server/src/Application/UseCases/Auth/AuthRegister/AuthRegisterUseCase.ts
@@ -3,16 +3,32 @@ import type { UseCase } from "@UseCases/UseCase";
 import type { AuthRegisterCommand } from "./AuthRegisterCommand";
 
 import { inject, injectable } from "inversify";
-import { UnitOfWork } from "@Database/UnitOfWork";
+import { PrismaClient } from "generated/prisma";
+
 import { AuthRegisterSchema } from "./AuthRegisterSchema";
+import { LogicException } from "@Exceptions/LogicException";
+import { TokenService } from "@Services/Token/TokenService";
+import { BcryptService } from "@Services/Bcrypt/BcryptService";
 
 @injectable()
 export class AuthRegisterUseCase implements UseCase<AuthRegisterCommand, AuthDTO> {
-  constructor(@inject(UnitOfWork) private readonly uow: UnitOfWork) {};
+  constructor(@inject(PrismaClient) private readonly prisma: PrismaClient) {};
 
   public async execute(command: AuthRegisterCommand): Promise<AuthDTO> {
     const validated = await AuthRegisterSchema.validate(command);
 
-    throw new Error("Method not implemented.");
+    const existing = await this.prisma.user.findFirst({
+      where: { name: validated.name, email: validated.email }
+    });
+
+    if (existing) throw new LogicException.Redundancy("User already exists");
+    const hashed = await BcryptService.hash(validated.password);
+
+    const created = await this.prisma.user.create({
+      data: { ...validated, password: hashed }
+    });
+
+    const token = await TokenService.sign({ user: created.id, expi: "30m" });
+    return { type: "Bearer", token };
   };
 };
