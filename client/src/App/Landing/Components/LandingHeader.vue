22de08f795f171108aad9d5a11cc8baf0748ba2d
roney ruiz <roneyruizrojas@gmail.com>
2025-07-28 23:00:00 -0600

    feat(client): working on circle landing page

---
+++ client/src/App/Landing/Components/LandingHeader.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Circle from '@Components/Shared/Atoms/Circle.vue';
+</script>
+
+<template>
+  <header class="">
+    <div>
+      <Circle :simple="false" size="small" />
+    </div>
+  </header>
+</template>
---
+++ client/src/App/Landing/LandingPage.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import LandingHeader from './Components/LandingHeader.vue';
+</script>
+
+<template>
+  <div class="min-h-screen bg-[#09090b] text-white">
+    <LandingHeader />
+  </div>
+</template>
